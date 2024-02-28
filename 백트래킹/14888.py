9042b48d432822cc79814750a8fef163bcdcf5c9
OldJinGyu <chwlsrb00225@naver.com>
2024-02-28 23:55:54 +0900

    백트래킹

---
+++ 백트래킹/14888.py
@@ -0,0 +1,23 @@
+import sys
+from itertools import permutations
+input=sys.stdin.readline
+n=int(input())
+nums=list(map(int,input().split()))
+op=list(map(int,input().split()))
+opl=['+']*op[0]+['-']*op[1]+['*']*op[2]+['/']*op[3]
+kings=permutations(opl)
+rs=[]
+for i in kings:
+    num=nums[0]
+    for j in range(n-1):
+        if i[j]=='+':
+            num+=nums[j+1]
+        elif i[j]=='-':
+            num-=nums[j+1]
+        elif i[j]=='*':
+            num*=nums[j+1]
+        elif i[j]=='/':
+            num//=nums[j+1]
+    rs.append(num)
+print(max(rs))
+print(min(rs))
