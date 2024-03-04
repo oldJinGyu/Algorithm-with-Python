cc8b7203ffb7f3a73cd360fffc7c55d07ee02f68
OldJinGyu <chwlsrb00225@naver.com>
2024-03-04 23:33:18 +0900

    백트레킹

--- 백트래킹/14888.py
+++ 백트래킹/14888.py
@@ -1,26 +1,31 @@
 import sys
-from itertools import permutations
-input=sys.stdin.readline
-n=int(input())
-nums=list(map(int,input().split()))
-op=list(map(int,input().split()))
-opl=['+']*op[0]+['-']*op[1]+['*']*op[2]+['/']*op[3]
-kings=permutations(opl)
-rs=[]
-for i in kings:
-    num=nums[0]
-    for j in range(n-1):
-        if i[j]=='+':
-            num+=nums[j+1]
-        elif i[j]=='-':
-            num-=nums[j+1]
-        elif i[j]=='*':
-            num*=nums[j+1]
-        elif i[j]=='/':
-            if num<0:
-                num=-(-num//nums[j+1])
-            else:
-                num//=nums[j+1]
-    rs.append(num)
-print(max(rs))
-print(min(rs))
+
+input = sys.stdin.readline
+N = int(input())
+num = list(map(int, input().split()))
+op = list(map(int, input().split()))  # +, -, *, //
+
+maximum = -1e9
+minimum = 1e9
+
+
+def dfs(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
+
+    if plus:
+        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
+    if minus:
+        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
+    if multiply:
+        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
+    if divide:
+        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
+
+
+dfs(1, num[0], op[0], op[1], op[2], op[3])
+print(maximum)
+print(minimum)
