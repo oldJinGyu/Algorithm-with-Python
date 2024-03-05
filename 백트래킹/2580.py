98d201c05766f18513dcf9786c90ff795655e557
OldJinGyu <chwlsrb00225@naver.com>
2024-03-06 00:21:44 +0900

    백트레킹 완

--- 백트래킹/2580.py
+++ 백트래킹/2580.py
@@ -1,32 +1,40 @@
 import sys
 input=sys.stdin.readline
 lst=[list(map(int,input().split())) for _ in range(9)]
-v1=[[False]*9 for _ in range(9)] #열
-v2=[[False]*9 for _ in range(9)] #행
-v3=[[False]*9 for _ in range(9)] #박스
-
+blank=[]
 for i in range(9):
     for j in range(9):
-        if lst[i][j]!=0:
-            v1[i][lst[i][j]-1]=True
-            v2[j][lst[i][j]-1]=True
-            v3[i//3*3+j//3][lst[i][j]-1]=True
-
-def back(i,j):
-    if i==9:
-        for x in lst:
-            print(*x)
+        if lst[i][j] == 0:
+            blank.append((i, j))
+def checkrow(x, num):
+    for i in range(9):
+        if num==lst[x][i]:
+            return False
+    return True
+def checkcol(x,num):
+    for i in range(9):
+        if num==lst[i][x]:
+            return False
+    return True
+def checkbox(x,y,num):
+    for i in range(3):
+        for j in range(3):
+            if num==lst[x//3*3+i][y//3*3+j]:
+                return False
+    return True
+def dfs(n):
+    if n==len(blank):
+        for i in lst:
+            print(*i)
         return
-    if lst[i][j]:
-        back(i+(j+1)//9,(j+1)%9)
-        return
-    for k in range(9):
-        if v1[i][k] or v2[j][k] or v3[i//3*3+j//3][k]:
-            continue
-        lst[i][j] = k+1
-        v1[i][k]=v2[j][k]=v3[i//3*3+j//3][k]=True
-        back(i+(j+1)//9,(j+1)%9)
-        lst[i][j] = 0
-        v1[i][k]=v2[j][k]=v3[i//3*3+j//3][k]=False
-
-back(0,0)
+    
+    for i in range(1,10):
+        x=blank[n][0]
+        y=blank[n][1]
+        
+        if checkbox(x,y,i) and checkcol(y,i) and checkrow(x,i):
+            lst[x][y]=i
+            dfs(n+1)
+            lst[x][y]=0
+            
+dfs(0)
