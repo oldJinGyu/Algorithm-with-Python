a0c9e77dae524aa49e7738af70e457aa20830dee
OldJinGyu <chwlsrb00225@naver.com>
2024-03-12 23:21:05 +0900

    bfs/dfs

---
+++ programmers/DFS,BFS/여행경로.py
@@ -0,0 +1,33 @@
+import sys
+input=sys.stdin.readline
+
+tickets=[]
+for _ in range(int(input())):
+    start,end=input().split()
+    tickets.append([start,end])
+            
+def solution(tickets):
+    tickets=sorted(tickets, key=lambda x:x[1])
+    answer = []
+    answer.append('ICN')
+    tlen=len(tickets)
+    def dfs(n,tickets,start,lst,answer):
+        print(*lst)
+        print()
+        if n==tlen:
+            answer+=lst
+            return True
+        for i in range(len(tickets)):
+            if start==tickets[i][0]:
+                s,e=tickets[i][0],tickets[i][1]
+                tickets.pop(i)
+                lst.append(e)
+                if dfs(n+1,tickets,e,lst,answer):
+                    return True
+                tickets.insert(i,[s,e])
+                lst.pop(-1)
+        return False
+    dfs(0,tickets,'ICN',[],answer)
+    return answer
+
+print(solution(tickets))
